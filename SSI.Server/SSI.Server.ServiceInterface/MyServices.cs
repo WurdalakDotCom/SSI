ae630e22d3bd36e0be64799304cf51489e9298de
v.lyapich <vladvlad.45@mail.ru>
2019-11-24 14:37:57 +0300

    Добавьте файлы проекта.

---
+++ SSI.Server/SSI.Server.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using SSI.Server.ServiceModel;
+
+namespace SSI.Server.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
---
+++ SSI.Server/SSI.Server/AppHost.cs
@@ -0,0 +1,44 @@
+﻿using Funq;
+using ServiceStack;
+using ServiceStack.Auth;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using SSI.Server.ServiceInterface;
+using System.Configuration;
+
+namespace SSI.Server
+{
+    //VS.NET Template Info: https://servicestack.net/vs-templates/EmptySelfHost
+    public class AppHost : AppSelfHostBase
+    {
+        /// <summary>
+        /// Base constructor requires a Name and Assembly where web service implementation is located
+        /// </summary>
+        public AppHost()
+            : base("SSI.Server", typeof(MyServices).Assembly) { }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        public override void Configure(Container container)
+        {
+            var dbFactory = container.Register<IDbConnectionFactory>(c =>
+                new OrmLiteConnectionFactory(ConfigurationManager.AppSettings.Get("IDB"), PostgreSqlDialect.Provider));
+
+            Plugins.Add(new AuthFeature(
+                    () => new AuthUserSession()
+                    , new IAuthProvider[]
+                    {
+                        new CredentialsAuthProvider() { SessionExpiry = System.TimeSpan.FromHours(AppSettings.Get<int>("SessionLifeTimeInHours")) }
+                    }
+                ));
+
+
+            container.Register<IAuthRepository>(arg =>
+                new OrmLiteAuthRepository(container.Resolve<IDbConnectionFactory>()) { UseDistinctRoleTables = true, ForceCaseInsensitiveUserNameSearch = true });
+
+            container.Resolve<IAuthRepository>().InitSchema();
+        }
+    }
+}
